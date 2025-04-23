890acc04cb8f58cdc565765130ed85db889cebb2
AkapongAlone <petinw@gmail.com>
2025-04-23 17:26:34 +0700

    update: book feature

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from .models import Book
@@ -7,3 +8,6 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['genre', 'title', 'publisher','is_active']
